235c067c9197d5cfac2eef06128c11c86554c9a5
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-15 23:20:11 -0500

    Int to String change
    Want to run this but changing the data type

--- An_interesting_loop_2.py
+++ An_interesting_loop_2.py
@@ -1,7 +1,8 @@
 #An_interesting_loop_2
 numlist = list()
 while True :
-    inp = input('Enter a number: ')
+ //   inp = input('Enter a number: ')
+    inp = input('Enter a String: ')
     if inp == 'done' : break
     value = float(inp)
     numlist.append(value)
